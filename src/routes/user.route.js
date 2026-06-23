c3cbce01add0fe0211aef78dc96bd57b0837b5fd
unknown <pspatil19874@gmail.com>
2026-06-23 18:10:24 +0530

    tokn done

--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -1,6 +1,7 @@
 import { Router } from "express";
-import { registeruser } from "../controllers/user.controller.js";
+import { registeruser, loginuser,logoutuser,refreshAccessToken } from "../controllers/user.controller.js";
 import { upload } from "../middleware/multer.middleware.js"
+import { verifyJWT } from "../middleware/auth.middleware.js"
 const router = Router()
 
 router.route("/register").post(
@@ -14,5 +15,11 @@ router.route("/register").post(
         }
     ]), registeruser
 )
+
+router.route("/login").post(loginuser)
+
+//secured routes
+router.route("/logout").post(verifyJWT,  logoutuser)
+router.route("/refresh-token").post(refreshAccessToken)
 
 export default router
